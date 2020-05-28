cce0c81592d73f530edc12edb5241c069e11a66e
LeuAndreea <45315422+LeuAndreea@users.noreply.github.com>
2020-05-28 18:24:33 +0300

    .

---
+++ 15_server.c
@@ -0,0 +1,55 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //creating server and client pipes
+    if (mkfifo("SERVER",0666) < 0){
+        perror("Error creating SERVER!");
+        return 0;
+    }
+
+    if (mkfifo("CLIENT",0666) < 0){
+        perror("Error creating client!");
+        return 0;
+    }
+
+    //opening the pipes
+    int pipe_server= open("SERVER", O_WRONLY );
+    int pipe_client= open("CLIENT", O_RDONLY );
+    
+    
+    
+    fflush(stdin);
+    
+    //getting file name from user
+    char file_name[20];
+    size_t size;
+    size =read(pipe_client, file_name,20);
+
+
+    //composing the instruction for counting lines
+    //+redirecting output to client
+    char instruction[30]= "wc -l ";
+    strncat(instruction, file_name, size);
+    strcat(instruction," >> SERVER ");
+
+    //had to do it this way, redirectin stderr did not send full error
+    if(system(instruction) > 0)
+	    write(pipe_server,"Error opening file!\n",20);
+
+  
+    //deleting links
+    remove("SERVER");
+    remove("CLIENT");
+    //closing pipes
+    close(pipe_server);
+    close(pipe_client);
+
+    //return 0;
+}
---
+++ 16_client.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //opening file pipes
+    int pip_server =open("SERVER",O_RDONLY );
+    int pip_client =open("CLIENT",O_WRONLY ) ;
+
+  
+    fflush(stdin);
+   
+  
+    //getting file name from the client
+    printf("Please introduce a file name: ");
+    
+    char *file_name=NULL;
+    size_t size,line_size;
+    line_size=getline(&file_name,&size,stdin);
+
+    fflush(stdin);
+    write(pip_client,file_name,line_size-1);
+
+    //getting back nb of words from server
+    char message[50];
+    size=read(pip_server,&message,50);
+     
+    write(1,message,size);
+    printf("\n");
+
+    close(pip_server);
+    close(pip_client);
+}
